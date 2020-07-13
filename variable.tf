9aaba2765a93c576c1237d4bd294bd919fd1ac01
Tofan Jena <Tofanjena100@Gmail.com>
2020-07-13 23:59:53 +0530

    okok

--- variable.tf
+++ variable.tf
@@ -14,16 +14,7 @@ variable "counts" {}
  variable "prefix-nic" {}
 # Prefix for PIP
  variable "prefix-pip" {}
-# Customer1 - SP Details - NEWRDS
-variable "client_id_C1" {}
-variable "client_secret_C1" {}
-variable "tenant_id_C1" {} 
-variable "subscription_id_C1" {}
-# Customer2 - SP Details -RDSLAB
-variable "client_id_C2" {}
-variable "client_secret_C2" {} 
-variable "tenant_id_C2" {}
-variable "subscription_id_C2" {}
+
  ## VNet Address Space for C1
  variable "vnet-c1" {}
 ## VNet Address Space for C2
